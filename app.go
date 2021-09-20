e1cb8b5660058fec2967d2db027d61e25ca05e92
alaref-codes <alarefabdo1@gmail.com>
2021-09-20 21:58:49 +0200

    first commit

---
+++ app.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/alaref-codes/basic-auth/api/user"
+	"github.com/alaref-codes/basic-auth/api/database"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+
+	jwtware "github.com/gofiber/jwt/v3"
+
+	"gorm.io/gorm"
+)
+
+func main() {
+	app := fiber.New()
+	setUpRoutes(app)
+	initDatabase()
+	log.Fatal(app.Listen(":3000"))
+}
+
+func initDatabase() {
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
+	database.DBConn = db
+	if err != nil {
+		panic("failed to connect database")
+	}
+	database.DBConn.AutoMigrate(&user.User{})
+}
+
+func setUpRoutes(app *fiber.App) {
+	app.Post("/users/signin", user.Signin)
+	app.Post("/users", user.CreateUser) // Sign up page
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
+
+	// Restricted routes !
+	app.Get("/users", user.GetUsers)
+
+	app.Delete("/users/:email", user.DeleteUser)
+
+}
---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/alaref-codes/basic-auth
+
+go 1.17
+
+require (
+	github.com/gofiber/fiber/v2 v2.18.0
+	github.com/gofiber/jwt/v3 v3.1.0
+	github.com/golang-jwt/jwt/v4 v4.0.0
+	golang.org/x/crypto v0.0.0-20210920023735-84f357641f63
+	gorm.io/driver/sqlite v1.1.5
+	gorm.io/gorm v1.21.15
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.29.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
